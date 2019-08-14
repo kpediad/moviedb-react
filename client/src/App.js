6f0938e80050a56e21f8b4ce85573e98f8a55e2d
kpediad <pediadk@gmail.com>
2019-08-14 16:12:18 +0300

    Incorporates react-router in client app

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
