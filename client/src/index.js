159964b01a05bbc723eafa0960782f61edc22689
kpediad <pediadk@gmail.com>
2019-09-11 19:20:33 +0300

    Incorporates store so that skeleton project works

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 import App from './App';
+
+import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {
+  movies: [
+    { id: 1, title: 'A River Runs Through It' },
+    { id: 2, title: 'Se7en' },
+    { id: 3, title: 'Inception' }
+  ]
+};
+
+const store = createStore(rootReducer, initialState);
+
+
+
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
