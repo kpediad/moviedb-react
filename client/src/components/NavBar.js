e8d076d71f5df16ecad947bb1fc82999abc376a8
kpediad <pediadk@gmail.com>
2019-09-11 23:04:16 +0300

    Finalizes style for navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilm } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = props => {
   return (
     <nav className="navbar navbar-expand-sm bg-primary navbar-dark">
-      <a className="navbar-brand" href="/"><span className="fa fa-film"></span>MovieDB</a>
-
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a class="nav-link" href="/movies">Movies</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/movies/new">Add Movie</a>
-        </li>
-      </ul>
-
-  </nav>
-
-
-
+      <NavLink className="navbar-brand" to="/"><FontAwesomeIcon icon={faFilm} /> MovieDB</NavLink>
+      <NavLink className="nav-link text-light" to="/movies">Movies</NavLink>
+      <NavLink className="nav-link text-light" to="/movies/new">Add Movie</NavLink>
+    </nav>
   );
 }
 
