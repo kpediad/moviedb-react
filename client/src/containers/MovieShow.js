81781d83fe41b8a376dae8d0ddf46cc3d7068a09
kpediad <pediadk@gmail.com>
2019-09-12 17:16:34 +0300

    Adopts MovieShow container

--- client/src/containers/MovieShow.js
+++ client/src/containers/MovieShow.js
@@ -2,9 +2,30 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const MovieShow = ({ movie }) =>
-  <div>
-    <h3>Title: {movie.title}</h3>
-  </div>
+  <div className="container" >
+    <div className="row justify-content-center">
+      <table className="table bg-light" style={{textAlign: 'center'}}>
+        <thead>
+          <tr className="table-primary">
+            <th colspan="3" scope="col" id="movie">{movie.title}</th>
+          </tr>
+          <tr>
+            <th id="year">Year: {movie.release_year}</th>
+            <th id="score">Score: </th>
+          </tr>
+          <tr className="table-primary" style={{textAlign: 'center'}}>
+            <th colspan="3" scope="col">Synopsis</th>
+          </tr>
+          <tr style={{textAlign: 'justify'}}>
+            <td colspan="3" id="synopsis">{movie.synopsis}</td>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    </div>
+  </div>;
+
 
 const mapStateToProps = (state, ownProps) => {
   const movie = state.movies.find(movie => movie.id == ownProps.match.params.movieId)
