86beae8b4ee9c703b002236f45442d2f916d3260
kpediad <pediadk@gmail.com>
2019-09-13 13:48:14 +0300

    Corrects warnings in MovieList component

--- client/src/components/MoviesList.js
+++ client/src/components/MoviesList.js
@@ -3,7 +3,7 @@ import MoviesListItem from './MoviesListItem';
 
 const MoviesList = ({ movies }) => {
   const renderTableRows = movies.map(movie =>
-      <MoviesListItem movie={movie} />
+      <MoviesListItem key={movie.id} movie={movie} />
   );
 
   return (
