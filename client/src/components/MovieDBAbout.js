f924e4a3a9a0cc97f75b42eb63b189f3d2e40efb
kpediad <pediadk@gmail.com>
2019-09-13 15:41:31 +0300

    Adds final content for About page

--- client/src/components/MovieDBAbout.js
+++ client/src/components/MovieDBAbout.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilm } from '@fortawesome/free-solid-svg-icons';
+
 
 const MovieDBAbout = props =>
-  <div>
-    <h2>MovieDB About Page</h2>
-    <p>Welcome to the MovieDB App!</p>
-    <p>DUMMY PAGE ! Add About page Info!</p>
-  </div>;
+  <div className="container">
+    <div className="row justify-content-center">
+      <div className="col-6">
+        <div className="card text-white bg-primary mb-3" style={{textAlign: 'center'}}>
+          <h3 className="card-header"><FontAwesomeIcon icon={faFilm} /> MovieDB</h3>
+          <img src="/moviedb.jpg" alt="MovieDB image" />
+        </div>
+      </div>
+      <div className="col-6 align-self-center">
+        <div className="card text-black bg-light mb-3" style={{textAlign: 'center'}}>
+          <h3 className="card-header">About MovieDB</h3>
+          <p className="card-body"><strong>Version 0.3</strong><br/><br/>Single Page Application<br/>Powered by ReactJS & Redux<br/><br/>Use links at the top bar to navigate the app. Have Fun!<br/><br/>© 2019 Konstantinos (Kostas) Pediaditakis</p>
+        </div>
+      </div>
+    </div>
+  </div>
 
 export default MovieDBAbout
