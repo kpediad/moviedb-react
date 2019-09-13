5b0ddec4a32a4e092014c47b19dcced00ef61e21
kpediad <pediadk@gmail.com>
2019-09-13 15:13:20 +0300

    Adds final content for Home page

--- client/src/components/MovieDBHome.js
+++ client/src/components/MovieDBHome.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilm } from '@fortawesome/free-solid-svg-icons';
 
 const MovieDBHome = props =>
-  <div>
-    <h2>MovieDB Home Page</h2>
-    <p>Welcome to the MovieDB App!</p>
-    <p>DUMMY PAGE ! ADD MORE CONTENT AND FORMATING HERE !</p>
-  </div>;
+  <div className="container">
+    <div className="row justify-content-center">
+      <div className="card text-white bg-primary mb-3" style={{textAlign: 'center'}}>
+        <h3 className="card-header"><FontAwesomeIcon icon={faFilm} /> MovieDB</h3>
+        <img src="/moviedb.jpg" alt="MovieDB image" />
+        <div className="card-body">
+          <p className="card-text">Rate you favorite and not so favorite movies...</p>
+        </div>
+      </div>
+    </div>
+  </div>
 
 export default MovieDBHome
