bb5e7cb5b29d34f560689335122340e33d26859a
kpediad <pediadk@gmail.com>
2019-09-13 17:49:02 +0300

    Adds db migration for api server

---
+++ db/migrate/20190512085654_create_movies.rb
@@ -0,0 +1,12 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :release_year
+      t.text :synopsis
+      t.integer :score
+
+      t.timestamps
+    end
+  end
+end
